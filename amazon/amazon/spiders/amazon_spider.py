402791c85858ea81729d570c040ea80d89d9bb20
kenkpix <44982150+kenkpix@users.noreply.github.com>
2020-04-25 16:18:21 +0300

    implement amazonlaps spider

--- amazon/amazon/spiders/amazon_spider.py
+++ amazon/amazon/spiders/amazon_spider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import pandas as pd
 
 
 class AmazonLink(scrapy.Spider):
@@ -13,11 +14,35 @@ class AmazonLink(scrapy.Spider):
             yield {'link': link}
 
 
-# class AmazonData(scrapy.Spider):
-#     name = 'amazon_laptops'
-#     allowed_domains = ['https://www.amazon.com/']
+class AmazonData(scrapy.Spider):
+    name = 'amazon_laptops'
+    allowed_domains = ['https://www.amazon.com/']
 
-#     with open
+    with open('/home/vladislav/GitHub/laptops_scrapy/amazon/output.json', 'r') as f:
+        links = json.load(f)
 
-#     start_urls = []
+    df = pd.DataFrame(links)
+
+    to_drop = ["/electronics-store/b?ie=UTF8&node=172282",
+           "/computer-pc-hardware-accessories-add-ons/b?ie=UTF8&node=541966",
+           "/Computers-Tablets/b?ie=UTF8&node=13896617011"]
+    
+    for link in to_drop:
+        df = df.drop(df[df.link == link].index)   
+
+    df = df[~df['link'].str.startswith('https://')]
+    df = df.drop_duplicates()
+    # # TEMP
+    # testing = df.iloc[0:5]
+    # # TEMP
+    start_urls = ['https://www.amazon.com' + str(i) for i in df.link.values]
+
+    def parse(self, response):
+        for vals in response.xpath("(//table[@class='a-keyvalue prodDetTable'])[1]"):
+            yield {
+                'values': vals.xpath("//td[@class='a-size-base']/text()").getall(),
+            }
+
+
+
     
